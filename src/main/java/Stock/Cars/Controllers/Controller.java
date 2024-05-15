d2cdd8dcfdb2998016219a771b9384af9c93fbf7
MohcinProject <ZahdiMohcine20@gmail.com>
2024-05-15 11:01:20 +0200

    finish the CRUD API

--- src/main/java/Stock/Cars/Controllers/Controller.java
+++ src/main/java/Stock/Cars/Controllers/Controller.java
@@ -5,13 +5,14 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import Stock.Cars.Model.Car;
 import Stock.Cars.Services.Service;
-import jakarta.websocket.server.PathParam;
 
 @RestController
 
@@ -37,8 +38,13 @@ public class Controller {
         carsService.registerNewCar(car);
     }
 
-    @DeleteMapping(path="{id}")
-    public void deleteCar(@PathParam("id") Long id) {
+    @DeleteMapping(path="/car/delete_{id}")
+    public void deleteCar(@PathVariable("id") Long id) {
         carsService.deleteCar(id);
+    }
+
+    @PutMapping(path = "car/update_{id}")
+    public void updateCar(@PathVariable("id") Long id, @RequestBody Car car) {
+        carsService.updateCar(id, car);
     }
 }
--- src/main/java/Stock/Cars/Services/Service.java
+++ src/main/java/Stock/Cars/Services/Service.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import Stock.Cars.Model.Car;
 import Stock.Cars.repisitory.CarsStock;
+import jakarta.transaction.Transactional;
 
 @org.springframework.stereotype.Service
 public class Service {
@@ -43,6 +44,22 @@ public class Service {
             throw new IllegalStateException("no car with the id : " + id) ;
         }
         carsStock.deleteById(id); 
+    }
+
+
+    @Transactional
+    public void updateCar(Long id , Car car) {
+        Optional<Car> optionalCar = carsStock.findById(id ) ; 
+        if (!optionalCar.isPresent()) {
+            throw new IllegalStateException("no car with the id : " + id) ;
+        }
+        Car NewCar = optionalCar.get() ; 
+        NewCar.setBrand(car.getBrand());
+        NewCar.setColor(car.getColor());
+        NewCar.setModel(car.getModel());
+        NewCar.setYear(car.getYear());
+        carsStock.save(NewCar);
+
     }   
 
 
